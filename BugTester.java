baec28a7c455fd3f3049eb9986472c07babd5bca
Ofek Gila <ofek@theofekfoundation.org>
2016-10-28 21:09:36 -0700

    Refactor code

--- BugTester.java
+++ BugTester.java
@@ -4,130 +4,104 @@ import java.util.Arrays;
  * Tests {@link OFile} for bugs.
  * Run with -ea parameter
  */
-public class BugTester {
+public class BugTester extends CodeTester {
 
-	private final String TEST_FILE_NAME;
-	private final String TEST_DIR_NAME;
-	private final String COPY_FILE_NAME;
-	private final String RENAMED_FILE_NAME;
+	public final String TEST_FILE_NAME;
+	public final String TEST_DIR_NAME;
+	public final String COPY_FILE_NAME;
+	public final String RENAMED_FILE_NAME;
 
-	private int testsOk, testsFail, testsTotal;
-
-	private OFile testFile;
+	public OFile testFile;
 
 	public BugTester(String fileName) {
 		TEST_FILE_NAME = fileName;
 		TEST_DIR_NAME = "test_dir";
 		COPY_FILE_NAME = "copy_" + fileName;
 		RENAMED_FILE_NAME = "renamed_" + fileName;
-		testsOk = testsFail = testsTotal = 0;
-		testFile = null;
+		runTests(this);
+		cleanupFile();
 	}
 
-	private void setupFile() {
+	public void setupFile() {
 		testFile = new OFile("tester.txt");
 	}
 
-	private void cleanupFile() {
+	public void cleanupFile() {
 		testFile.delete();
 		new OFile(COPY_FILE_NAME).delete();
 		new OFile(TEST_DIR_NAME + "/").delete();
 		new OFile(TEST_DIR_NAME + "2/").delete();
 	}
 
-	interface Test {
-		void test();
-	}
-
-	private void runTest(String testName, Test test) {
-		System.out.printf("%-50s --> ", testName);
-		try {
-			test.test();
-			System.out.println("ok");
-			testsOk++;
-		} catch (AssertionError ae) {
-			System.out.println("FAIL\n");
-			ae.printStackTrace();
-			System.out.println();
-			testsFail++;
-		} catch (Exception err) {
-			System.out.println("FAIL\n");
-			err.printStackTrace();
-			System.out.println();
-			testsFail++;
-		}
-		testsTotal++;
-	}
-
-	private void printSummary() {
-		if (testsFail > 0)
-			System.out.printf("!!!! %d test%s failed out of %d tests total.\n",
-				testsFail, testsFail == 1 ? "":"s", testsTotal);
-		else System.out.printf("Passed all %d tests!\n", testsTotal);
-	}
-
-	private void testCreateDelete(String fileName) {
+	private void _testFileCreationAndDeletion(String fileName) {
 		assertExists(fileName, false);
 
-		OFile testFile = new OFile(fileName);
+		OFile lokoFile = new OFile(fileName);
 
-		assertExists(testFile, true);
-		assertEmpty(testFile);
+		assertExists(lokoFile, true);
+		assertEmpty(lokoFile);
 
-		assertDelete(testFile);
+		assertDelete(lokoFile);
 	}
 
-	private void testCreateDeleteDirectory() {
+	public void testFileCreationAndDeletion() {
+		_testFileCreationAndDeletion("loko");
+		_testFileCreationAndDeletion("loko/");
+	}
+
+	public void testFileCreationAndDeletionWithinDirectory() {
 		String filePath = TEST_DIR_NAME + "/" + TEST_FILE_NAME;
 
 		assertExists(filePath, false);
 		assertExists(TEST_DIR_NAME, false);
 
-		testFile = new OFile(filePath);
+		OFile testDir = new OFile(filePath);
 
 		assertExists(filePath, true);
 		assertExists(TEST_DIR_NAME, true);
-		assertEmpty(testFile);
+		assertEmpty(testDir);
 
-		assertDelete(testFile, testFile.getParentFile());
+		assertDelete(testDir, testDir.getParentFile());
 	}
 
-	private void testReadWrite() {
+	public void testReadWrite() {
+		setupFile();
+
 		testFile.write("hello world");
 		testFile.write(" and goodnight moon.\n");
 		testFile.write("new line");
 
-		assertEqual(testFile.read(), "hello world and goodnight moon.");
-		assertEqual(testFile.read(), "new line");
+		assertEqual(testFile.read(), "hello world and goodnight moon.", true);
+		assertEqual(testFile.read(), "new line", true);
 		assertNull(testFile.read(), true);
 		assertEqual(testFile.readFile(),
-			"hello world and goodnight moon.\nnew line");
+			"hello world and goodnight moon.\nnew line", true);
 
 		assertClear(testFile);
 	}
 
-	private void testLargeReadWrite() {
+	public void testLargeReadWrite() {
 		int numLines = (int)1e4;
 		for (int i = 0; i < numLines; i++)
 			testFile.write(String.format("Line Num: %d\n", (i + 1)));
 
-		assertEqual(countCharacters(testFile.readFile(), '\n'), numLines);
+		assertEqual(countCharacters(testFile.readFile(), '\n'), numLines, true);
 
 		for (int i = 0; i < numLines; i++)
 			assertEqual(testFile.read(),
-				String.format("Line Num: %d", (i + 1)));
+				String.format("Line Num: %d", (i + 1)), true);
 
 		assertClear(testFile);
 	}
 
-	private void testFileCopy() {
+	public void testFileCopy() {
 		assertExists(COPY_FILE_NAME, false);
 
 		testFile.write("copy this");
 		OFile copiedFile = assertReplace(testFile, COPY_FILE_NAME);
 
-		assertEqual(copiedFile.readFile(), testFile.readFile());
-		assertEqual(copiedFile.getChecksum(), testFile.getChecksum());
+		assertEqual(copiedFile.readFile(), testFile.readFile(), true);
+		assertEqual(copiedFile.getChecksum(), testFile.getChecksum(), true);
 
 		// Test that changes in copy file result in changes in equality
 		copiedFile.clear();
@@ -141,7 +115,7 @@ public class BugTester {
 		assertClear(testFile);
 	}
 
-	private void testFileCopyIntoDirectory() {
+	public void testFileCopyIntoDirectory() {
 		String filePath = TEST_DIR_NAME + "/" + COPY_FILE_NAME;
 
 		assertExists(TEST_DIR_NAME, false);
@@ -149,14 +123,14 @@ public class BugTester {
 		testFile.write("something to copy");
 		OFile copiedFile = assertReplace(testFile, filePath);
 
-		assertEqual(copiedFile.readFile(), testFile.readFile());
-		assertEqual(copiedFile.getChecksum(), testFile.getChecksum());
+		assertEqual(copiedFile.readFile(), testFile.readFile(), true);
+		assertEqual(copiedFile.getChecksum(), testFile.getChecksum(), true);
 
 		assertDelete(copiedFile, copiedFile.getParentFile());
 		assertClear(testFile);
 	}
 
-	private void testDirectoryCopyWithFile() {
+	public void testDirectoryCopyWithFile() {
 		String file1Path = TEST_DIR_NAME + "/" + COPY_FILE_NAME;
 		String file2Path = TEST_DIR_NAME + "2/" + COPY_FILE_NAME;
 
@@ -184,7 +158,7 @@ public class BugTester {
 		assertDelete(TEST_DIR_NAME + "/", TEST_DIR_NAME + "2/");
 	}
 
-	private void testFileRenamingInDirectory() {
+	public void testFileRenamingInDirectory() {
 		String filePath = TEST_DIR_NAME + "/" + TEST_FILE_NAME;
 
 		assertExists(filePath, false);
@@ -194,12 +168,12 @@ public class BugTester {
 
 		OFile renamedFile = assertRenaming(file, RENAMED_FILE_NAME);
 		assertExists(file, false);
-		assertEqual(renamedFile.readFile(), "text to preserve");
+		assertEqual(renamedFile.readFile(), "text to preserve", true);
 
 		assertDelete(renamedFile.getParentFile());
 	}
 
-	private void testDirectoryRenaming() {
+	public void testDirectoryRenaming() {
 		String filePath = TEST_DIR_NAME + "/" + TEST_FILE_NAME;
 
 		assertExists(TEST_DIR_NAME, false);
@@ -210,17 +184,17 @@ public class BugTester {
 		OFile tempFile = new OFile(filePath);
 		tempFile.write("text to preserve");
 		assertExists(filePath, true);
-		assertEqual(tempFile.readFile(), "text to preserve");
+		assertEqual(tempFile.readFile(), "text to preserve", true);
 
 		OFile renamedDir = assertRenaming(dir, TEST_DIR_NAME + "2");
 		assertExists(TEST_DIR_NAME + "2/" + TEST_FILE_NAME, true);
 		assertEqual(new OFile(TEST_DIR_NAME + "2/" + TEST_FILE_NAME).readFile(),
-			"text to preserve");
+			"text to preserve", true);
 
 		assertDelete(renamedDir);
 	}
 
-	private void testDirectoryCopying() {
+	public void testDirectoryCopying() {
 		String filePath = TEST_DIR_NAME + "/" + TEST_FILE_NAME;
 
 		assertExists(TEST_DIR_NAME, false);
@@ -231,7 +205,7 @@ public class BugTester {
 		OFile tempFile = new OFile(filePath);
 		tempFile.write("text to preserve");
 		assertExists(filePath, true);
-		assertEqual(tempFile.readFile(), "text to preserve");
+		assertEqual(tempFile.readFile(), "text to preserve", true);
 
 		OFile dir2 = assertReplace(dir, TEST_DIR_NAME + "2");
 		OFile dir3 = assertReplace(dir, TEST_DIR_NAME + "3/loko");
@@ -243,62 +217,18 @@ public class BugTester {
 		assertDelete(dir, dir2, dir3.getParentFile(), dir4.getParentFile());
 	}
 
-	public void runTests() {
-		System.out.println();
-
-		runTest("Test file creation and deletion", () -> testCreateDelete(TEST_FILE_NAME));
-		runTest("Test directory creation and deletion", () -> testCreateDelete(TEST_DIR_NAME + "/"));
-		runTest("Test file creation and deletion within directory", () ->
-			testCreateDeleteDirectory());
-
-		setupFile();
-
-		runTest("Test read write", () -> testReadWrite());
-		runTest("Test large read write", () -> testLargeReadWrite());
-		runTest("Test file copying", () -> testFileCopy());
-		runTest("Test file copying into a directory", () ->
-			testFileCopyIntoDirectory());
-		runTest("Test directory copying with file", () ->
-			testDirectoryCopyWithFile());
-		runTest("Test file renaming in a directory", () ->
-			testFileRenamingInDirectory());
-		runTest("Test directory renaming with file", () ->
-			testDirectoryRenaming());
-		runTest("Test directory copying", () ->
-			testDirectoryCopying());
-
-		cleanupFile();
-
-		System.out.println();
-		printSummary();
-		System.out.println();
+	public void assertEqual(byte[] b1, byte[] b2, boolean equal) {
+		assert Arrays.equals(b1, b2) == equal :
+			String.format("Byte arrays%s equal!", equal ? " not":"");
 	}
 
-	private void assertEqual(String s1, String s2) {
-		assert s1.equals(s2) :
-			String.format("\"%s\" != \"%s\"", s1, s2);
-	}
-
-	private void assertEqual(int i1, int i2) {
-		assert i1 == i2 : String.format("%d != %d!", i1, i2);
-	}
-
-	private void assertEqual(byte[] b1, byte[] b2) {
-		assert Arrays.equals(b1, b2) :
-			"Byte arrays not equal!";
-	}
-
-	private void assertEqual(OFile f1, OFile f2, boolean equal) {
+	public void assertEqual(OFile f1, OFile f2, boolean equal) {
 		assert f1.equalsIgnoreName(f2) == equal :
 			String.format("%s %s %s", f1.getPath(), equal ? "!=":"==",
 			f2.getPath());
 	}
 
-	private void assertNull(Object o, boolean nll) {
-		assert (o == null) == nll : o + " should be null!";
-	}
-
-	private void assertEmpty(OFile file) {
+	public void assertEmpty(OFile file) {
 		if (file.isDirectory())
 			assert file.listFiles().length == 0 :
 				String.format("Directory %s is not empty.", file.getPath());
@@ -306,25 +236,25 @@ public class BugTester {
 			String.format("File %s is not empty.", file.getPath());
 	}
 
-	private void assertExists(String path, boolean exists) {
+	public void assertExists(String path, boolean exists) {
 		assert OFile.fileExists(path) == exists :
 			String.format("File %s %s!", path,
 				exists ? "does not exist":"already exists");
 	}
 
-	private void assertExists(OFile file, boolean exists) {
+	public void assertExists(OFile file, boolean exists) {
 		assert file.exists() == exists :
 			String.format("File %s %s!", file.getPath(),
 				exists ? "does not exist":"already exists");
 		assertExists(file.getPath(), exists);
 	}
 
-	private void assertClear(OFile file) {
+	public void assertClear(OFile file) {
 		assert file.clear().readFile().length() == 0 :
 			String.format("Error clearing file %s!", file.getPath());
 	}
 
-	private void assertDelete(OFile... files) {
+	public void assertDelete(OFile... files) {
 		for (int i = 0; i < files.length; i++) {
 			assert files[i].delete() :
 			String.format("Error deleting file %s!", files[i].getPath());
@@ -332,12 +262,12 @@ public class BugTester {
 		}
 	}
 
-	private void assertDelete(String... paths) {
+	public void assertDelete(String... paths) {
 		for (int i = 0; i < paths.length; i++)
 			assertDelete(new OFile(paths[i]));
 	}
 
-	private OFile assertRenaming(OFile file, String newName) {
+	public OFile assertRenaming(OFile file, String newName) {
 		OFile renamedFile = file.renameTo(newName);
 		assert renamedFile != null:
 			String.format("Error renaming file %s!", file.getPath());
@@ -345,7 +275,7 @@ public class BugTester {
 		return renamedFile;
 	}
 
-	private OFile assertReplace(OFile file, String newPath) {
+	public OFile assertReplace(OFile file, String newPath) {
 		OFile replacedFile = file.copyReplace(newPath);
 		assert replacedFile != null:
 			String.format("Error replacing file %s!", file.getPath());
@@ -355,7 +285,7 @@ public class BugTester {
 		return replacedFile;
 	}
 
-	private int countCharacters(String s, char c) {
+	public int countCharacters(String s, char c) {
 		int count = 0, len = s.length();
 		for (int i = 0; i < len; i++)
 			if (s.charAt(i) == c)
@@ -365,6 +295,5 @@ public class BugTester {
 
 	public static void main(String... pumpkins) {
 		BugTester BT = new BugTester("tester.txt");
-		BT.runTests();
 	}
 }
---
+++ CodeTester.java
@@ -0,0 +1,99 @@
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public abstract class CodeTester {
+
+	public static final String TEST_METHOD_PREFIX = "test";
+	private int testsOk, testsFail, testsError, testsTotal;
+
+	public CodeTester() {
+		testsOk = testsFail = testsError = testsTotal = 0;
+	}
+
+	public void runTest(CodeTester childTester, Method testMethod) {
+		System.out.printf("%-50s --> ", parseMethodName(testMethod.getName()));
+		try {
+			testMethod.invoke(childTester);
+			System.out.println("ok");
+			testsOk++;
+		} catch (IllegalAccessException iae) {
+			System.out.println("ERR\n");
+			iae.printStackTrace();
+			System.out.println();
+			testsError++;
+		} catch (AssertionError ae) {
+			System.out.println("FAIL\n");
+			ae.printStackTrace();
+			System.out.println();
+			testsFail++;
+		} catch (Exception err) {
+			System.out.println("FAIL\n");
+			err.printStackTrace();
+			System.out.println();
+			testsFail++;
+		}
+		testsTotal++;
+	}
+
+	public void printSummary() {
+		System.out.println();
+		if (testsFail > 0)
+			System.out.printf("!!!! %d test%s failed out of %d tests total.\n",
+				testsFail, testsFail == 1 ? "":"s", testsTotal);
+		else System.out.printf("Passed all %d tests!\n", testsTotal);
+		System.out.println();
+	}
+
+	public final void runTests(CodeTester childTester) {
+		Class c = childTester.getClass();
+		for (Method method : c.getDeclaredMethods())
+			if (isTestMethod(method))
+				runTest(childTester, method);
+		printSummary();
+	}
+
+	private boolean isTestMethod(Method method) {
+		String methodName = method.getName();
+		int prefixLength = TEST_METHOD_PREFIX.length();
+
+		return methodName.length() > prefixLength + 1 &&
+			methodName.substring(0, prefixLength).equals(TEST_METHOD_PREFIX) &&
+			Character.isUpperCase(methodName.charAt(prefixLength)) &&
+			Modifier.isPublic(method.getModifiers());
+	}
+
+	private String parseMethodName(String methodName) {
+		String testName = "Test";
+		char tempChar;
+
+		for (int i = TEST_METHOD_PREFIX.length(); i < methodName.length(); i++) {
+			tempChar = methodName.charAt(i);
+			if (Character.isUpperCase(tempChar))
+				testName += " ";
+			testName += Character.toLowerCase(tempChar);
+		}
+
+		return testName;
+	}
+
+	public void assertEqual(String s1, String s2, boolean equal) {
+		assert s1.equals(s2) == equal :
+			String.format("\"%s\" %s \"%s\"", s1, equal ? "!=":"=", s2);
+	}
+
+	public void assertEqual(int i1, int i2, boolean equal) {
+		assert (i1 == i2) == equal :
+			String.format("%d %s %d!", i1, equal ? "!=":"=", i2);
+	}
+
+	public void assertNull(Object obj, boolean nll) {
+		assert (obj == null) == nll :
+			String.format("%s %s null.", obj.toString(), nll ? "!=":"=");
+	}
+
+	public void assertEqual(Object obj1, Object obj2, boolean equal) {
+		assert obj1.equals(obj2) == equal :
+			String.format("%s %s %s", obj1.toString(), equal ? "!=":"=",
+				obj2.toString());
+	}
+}
